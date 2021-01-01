820af1245aaeba352e1f0b92899119a1e984f2c4
Victor Abeledo <abeledovictor@gmail.com>
2021-01-01 12:11:08 -0300

    add initial example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {
+  AmpStory,
+  AmpStoryPage,
+  AmpStoryGridLayer,
+  AmpImg,
+  AmpHeader,
+  AmpParagraph,
+} from 'react-google-stories'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Cover page
+    <AmpStory standalone
+        title="Joy of Pets"
+        publisher="react-google-stories tutorial"
+        publisher-logo-src="assets/AMP-Brand-White-Icon.svg"
+        poster-portrait-src="assets/cover.jpg">
+      <AmpStoryPage id="cover">
+        <AmpStoryGridLayer template="fill">
+          <AmpImg src="assets/cover.jpg"
+              width="720" height="1280"
+              layout="responsive">
+          </AmpImg>
+        </AmpStoryGridLayer>
+        <AmpStoryGridLayer template="vertical">
+          <AmpHeader title="The Joy of Pets" level={1} />
+          <AmpParagraph text="By react-google-stories tutorials" />
+        </AmpStoryGridLayer>
+      </AmpStoryPage>
+
+      </AmpStory>
   );
 }
 
